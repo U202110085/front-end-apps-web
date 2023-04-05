784a735e7132f701aafb18f22184fefab82afae6
U202110085 <u202110085@upc.edu.pe>
2023-04-04 21:30:45 -0500

    update footer

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,19 @@
   <div>
     <Header /> <!-- Use the header component in your template -->
     <!-- Your app content goes here -->
+    <footer-component />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue' // Import the header component
+import FooterComponent from '@/components/Footer.vue';
 
 export default {
   components: {
-    Header // Register the header component in your Vue instance
+    FooterComponent,
+    Header
+     // Register the header component in your Vue instance
   },
   // Your app logic goes here
 }
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,17 +1,25 @@
 <template>
-  <div>
-    <!-- your main content here -->
-    <Footer />
-  </div>
+  <footer>
+    <p>&copy; 2023 My Website</p>
+  </footer>
 </template>
 
 <script>
-import Footer from '@/components/Footer.vue'
-
 export default {
-  name: 'App',
-  components: {
-    Footer,
-  },
+  name: 'FooterComponent',
 }
 </script>
+
+<style scoped>
+footer {
+    background-color: #333;
+    color: #fff;
+    padding: 20px;
+    text-align: center;
+}
+
+footer p {
+    margin: 0;
+    font-size: 14px;
+}
+</style>
