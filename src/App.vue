b7d1995c93b58da8c319a33782db0990e4ae317f
Leonel Ortega <u202115733@upc.edu.pe>
2023-04-05 12:25:41 -0500

    actualizaciones de las descripciones

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,20 @@
   <div>
     <Header /> <!-- Use the header component in your template -->
     <!-- Your app content goes here -->
-
-    <div class="container">
-      <div class="left-div">
-        <Category title="Category 1" description="ke hace"/>
-        <Category title="Category 2" description="ke hace"/>
-        <Category title="Category 3" description="ke hace"/>
-        <Category title="Category 4" description="ke hace"/>
+      <div class="container">
+          <div class="left-div">
+              <Category title="Category 1" description="Alumnos Pregrado"/>
+              <Category title="Category 2" description="Alumnos PostGrado"/>
+              <Category title="Category 3" description="Matricula"/>
+              <Category title="Category 4" description="Politica de pensiones"/>
+          </div>
+          <div class="right-div">
+              <Tutorial title="Tutoriales Chidos" imageUrl="https://elcomercio.pe/resizer/wtXe3Cbf-Mzeb2pfn2XQiskyL7k=/1200x1200/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/LVRUHV5RPZBWLIZD7Y2VLRNI3A.jpg" description="Como preparar almuerzo"/>
+              <Tutorial title="Reacciones" imageUrl="https://s.yimg.com/ny/api/res/1.2/WfAIcTxGIhQZIMOSjLPohw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTg1Mw--/https://media.zenfs.com/en/homerun/feed_manager_auto_publish_494/f4130c376e8c1633b900e6006fea8d89" description="Como preparar almuerzo"/>
+              <Tutorial title="Peticiones" imageUrl="https://cdn.memegenerator.es/imagenes/memes/full/0/27/277525.jpg" description="Como preparar almuerzo"/>
+              <Tutorial title="Lamentos" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRU1x8GAt38EPz0EXctHyXoGp6iT9gV96nZWQ&usqp=CAU" description="Como preparar almuerzo"/>
+          </div>
       </div>
-      <div class="right-div">
-        <Tutorial title="Tutorial 1" imageUrl="https://elcomercio.pe/resizer/wtXe3Cbf-Mzeb2pfn2XQiskyL7k=/1200x1200/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/LVRUHV5RPZBWLIZD7Y2VLRNI3A.jpg" description="Como preparar almuerzo"/>
-        <Tutorial title="Tutorial 2" imageUrl="https://s.yimg.com/ny/api/res/1.2/WfAIcTxGIhQZIMOSjLPohw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTg1Mw--/https://media.zenfs.com/en/homerun/feed_manager_auto_publish_494/f4130c376e8c1633b900e6006fea8d89" description="Como preparar almuerzo"/>
-        <Tutorial title="Tutorial 3" imageUrl="https://cdn.memegenerator.es/imagenes/memes/full/0/27/277525.jpg" description="Como preparar almuerzo"/>
-        <Tutorial title="Tutorial 4" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRU1x8GAt38EPz0EXctHyXoGp6iT9gV96nZWQ&usqp=CAU" description="Como preparar almuerzo"/>
-      </div>
-    </div>
-
     <footer-component></footer-component>
   </div>
 </template>
