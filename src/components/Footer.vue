604e06987c3a38326080e86342d31f78989a9023
U202110085 <u202110085@upc.edu.pe>
2023-04-05 10:38:33 -0500

    update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,15 @@
 <template>
   <footer>
     <nav>
-    <a href="mailto:venta@library.com">venta@library.com</a>
-    <p>981622558</p> 
-    <p>Los Manzanos 398</p>
+      <div class="left-section">
+        <a href="mailto:venta@library.com">venta@library.com</a>
+      </div>
+      <div class="middle-section">
+        <p>981622558</p>
+      </div>
+      <div class="right-section">
+        <p>Los Manzanos 398</p>
+      </div>
     </nav>
   </footer>
 </template>
@@ -19,8 +25,29 @@ footer {
     background-color: #333;
     color: #fff;
     padding: 20px;
-    justify-content: space-between;
     text-align: center;
+}
+
+nav {
+    display: flex;
+    justify-content: space-between;
+}
+
+nav > div {
+    display: flex;
+    align-items: center;
+}
+
+.left-section a {
+    margin-right: 20px;
+}
+
+.middle-section {
+    margin: 0 20px;
+}
+
+.right-section {
+    text-align: right;
 }
 
 footer p {
