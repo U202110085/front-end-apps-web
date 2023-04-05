7274aac17ed47f486ebebc60b1dad5d4ef9d182b
David Quito <112458588+Paronimus386@users.noreply.github.com>
2023-04-04 23:35:08 -0500

    Updated Footer withy text placed there
    In the Footer, added the e-mail link, phone number and address.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,10 @@
 <template>
   <footer>
-    <p>&copy; 2023 My Website</p>
+    <nav>
+    <a href="mailto:venta@library.com">venta@library.com</a></a> |
+    981622558</a> |
+    Los Manzanos 398</a>
+    </nav>
   </footer>
 </template>
 
@@ -15,6 +19,7 @@ footer {
     background-color: #333;
     color: #fff;
     padding: 20px;
+    justify-content: space-between;
     text-align: center;
 }
 
