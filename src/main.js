da82142dbe1b0498f89b8b6783558c1565c99432
U202110085 <u202110085@upc.edu.pe>
2023-04-11 20:03:48 -0500

    se actualizo el main y se implemento primevue

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+//Importing PrimeVue
+import PrimeVue from 'primevue/config';
+import "primevue/resources/themes/lara-light-indigo/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+
+//Importing PrimeVue Components
+const app = createApp(App);
+
+app.use(PrimeVue);
+
 createApp(App).mount('#app')
