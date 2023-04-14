1c38831622e83d7ffd5ee69df765dca080c7b92a
U202110085 <u202110085@upc.edu.pe>
2023-04-13 19:37:43 -0500

    implementacion de i18n

---
+++ src/locales/i18n.js
@@ -0,0 +1,26 @@
+import { createI18n } from "vue-i18n";
+
+const i18n = createI18n({
+  locale: "es",
+
+  messages: {
+    en: {
+      username: "User",
+      clickme: "Click me!",
+    },
+
+    es: {
+      username: "Usuario",
+      clickme: "Haz clic!",
+    },
+
+    fr: {
+      username: "Utilisateur",
+      clickme: "Cliquez sur moi!",
+    },
+  },
+
+  // something vue-i18n options here ...
+});
+
+export default i18n;
--- src/main.js
+++ src/main.js
@@ -1,36 +1,40 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
 //Importing PrimeVue
-import PrimeVue from 'primevue/config';
+import PrimeVue from "primevue/config";
 import "primevue/resources/themes/lara-light-indigo/theme.css";
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
 
-import Panel from 'primevue/panel';
-import Button from 'primevue/button';
-import InputText from 'primevue/inputtext';
-import InputMask from 'primevue/inputmask';
-import Dropdown from 'primevue/dropdown';
-import Calendar from 'primevue/calendar';
-import InputNumber from 'primevue/inputnumber';
-import Textarea from 'primevue/textarea';
-import Checkbox from 'primevue/checkbox';
-import RadioButton from 'primevue/radiobutton';
+import Panel from "primevue/panel";
+import Button from "primevue/button";
+import InputText from "primevue/inputtext";
+import InputMask from "primevue/inputmask";
+import Dropdown from "primevue/dropdown";
+import Calendar from "primevue/calendar";
+import InputNumber from "primevue/inputnumber";
+import Textarea from "primevue/textarea";
+import Checkbox from "primevue/checkbox";
+import RadioButton from "primevue/radiobutton";
+
+//Importing i18n
+import i18n from "./locales/i18n";
 
 //Importing PrimeVue Components
 const app = createApp(App);
 
-app.component('pv-button', Button);
-app.component('pv-input-text', InputText);
-app.component('pv-panel', Panel);
-app.component('pv-input-mask', InputMask);
-app.component('pv-dropdown', Dropdown);
-app.component('pv-calendar', Calendar);
-app.component('pv-input-number', InputNumber);
-app.component('pv-textarea', Textarea);
-app.component('pv-checkbox', Checkbox);
-app.component('pv-radio-button', RadioButton);
+app.component("pv-button", Button);
+app.component("pv-input-text", InputText);
+app.component("pv-panel", Panel);
+app.component("pv-input-mask", InputMask);
+app.component("pv-dropdown", Dropdown);
+app.component("pv-calendar", Calendar);
+app.component("pv-input-number", InputNumber);
+app.component("pv-textarea", Textarea);
+app.component("pv-checkbox", Checkbox);
+app.component("pv-radio-button", RadioButton);
 app.use(PrimeVue);
+app.use(i18n);
 
-app.mount('#app')
+app.mount("#app");
