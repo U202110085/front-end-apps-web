ba78d1d6b5f038f6625d848b1474537aef40bd66
U202110085 <u202110085@upc.edu.pe>
2023-04-26 11:09:25 -0500

    PostHome card update

--- src/components/PostHome.vue
+++ src/components/PostHome.vue
@@ -2,9 +2,14 @@
     <div class="user">
         <h1>Post</h1>
     </div>
-    <div v-for="post in posts" :key="post.id">
-        Post {{ post.id }} {{ post.username }}
-        <router-link :to="{ name: 'post', params: { id: post.id } }">Edit</router-link>
+    <div class="p-grid">
+        <div v-for="post in posts" :key="post.id" class="p-col-12 p-md-6 p-lg-4">
+            <div class="p-card">
+                <h3>{{ post.title }}</h3>
+                <p>{{ post.body }}</p>
+                <router-link :to="{ name: 'post', params: { id: post.id } }">Edit</router-link>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -31,4 +36,23 @@
     };
 </script>
 
-<style scoped></style>
+<style scoped>
+    .p-card {
+        border-radius: 4px;
+        box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
+        padding: 1rem;
+        margin-bottom: 1rem;
+        background-color: #fff;
+    }
+
+    .p-card h3 {
+        font-size: 1.2rem;
+        margin-bottom: 0.5rem;
+    }
+
+    .p-card p {
+        font-size: 1rem;
+        color: #555;
+        line-height: 1.4;
+    }
+</style>
